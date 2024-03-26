57c3cb160cb6674494cfc33874b0bed50d271129
Yadnyesh Khotre <96002336+yadnyeshkhotre@users.noreply.github.com>
2024-03-26 15:49:38 +0530

    Create row_of_max_ones.cpp

---
+++ arrays/row_of_max_ones.cpp
@@ -0,0 +1,60 @@
+/*
+Given a boolean 2D array of n x m dimensions, consisting of only 1's and 0's, where each row is sorted. Find the 0-based index of the first row that has the maximum number of 1's.
+
+Example 1:
+
+Input: 
+N = 4 , M = 4
+Arr[][] = {{0, 1, 1, 1},
+           {0, 0, 1, 1},
+           {1, 1, 1, 1},
+           {0, 0, 0, 0}}
+Output: 2
+Explanation: Row 2 contains 4 1's (0-based
+indexing).
+
+Example 2:
+
+Input: 
+N = 2, M = 2
+Arr[][] = {{0, 0}, {1, 1}}
+Output: 1
+Explanation: Row 1 contains 2 1's (0-based
+indexing).
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function rowWithMax1s() which takes the array of booleans arr[][], n and m as input parameters and returns the 0-based index of the first row that has the most number of 1s. If no such row exists, return -1.
+ 
+
+Expected Time Complexity: O(N+M)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N, M ≤ 103
+0 ≤ Arr[i][j] ≤ 1 
+
+
+question link:https://www.geeksforgeeks.org/problems/row-with-max-1s0023/1
+*/
+class Solution{
+public:
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    // code here
+	    int largest_ones=INT_MIN;
+	    int index=-1;
+	    for(int i=0;i<n;i++){
+	        int ones=0;
+	        for(int j=0;j<m;j++){
+	            if(arr[i][j]!=0){
+	                ones++;
+	            }
+	        }if(ones>largest_ones){
+	            largest_ones=ones;
+	            index=i;
+	        }
+	    }
+	    return (largest_ones==0)? -1:index;
+	}
+
+};
