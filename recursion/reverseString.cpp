178e24cbb1b9f2bb65a4a328017bf8f8f44f737b
Yadnyesh Khotre <96002336+yadnyeshkhotre@users.noreply.github.com>
2024-04-27 16:18:51 +0530

    Add files via upload

---
+++ recursion/reverseString.cpp
@@ -0,0 +1,21 @@
+void helper(vector<char> &s, int i)
+{
+    int n = s.size();
+    if (i >= (n / 2))
+    {
+        return;
+    }
+
+    swap(s[i], s[n - i - 1]);
+
+    helper(s, i + 1);
+}
+
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        helper(s, 0);
+    }
+};
